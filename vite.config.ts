471d2b652b17457f6a8b38f8e807e9395d2e5847
Станислав <szharov@breez.ru>
2026-02-10 13:30:25 +0300

    +test

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,6 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '') || '/',
       },
       '/uploads': {
         target: 'http://localhost:3001',
