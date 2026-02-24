3944876c7ea107d21090588f5f923e1a10416233
Станислав <szharov@breez.ru>
2026-02-24 09:18:46 +0300

    +logo

--- src/components/layout/header/HeaderLogo.vue
+++ src/components/layout/header/HeaderLogo.vue
@@ -1,7 +1,18 @@
 <template>
-  <router-link to="/dashboard" class="lg:hidden inline-block origin-center scale-50">
-    <img class="dark:hidden block" :src="logoUrl('logo.svg')" alt="Logo" />
-    <img class="hidden dark:block" :src="logoUrl('logo-dark.svg')" alt="Logo" />
+  <router-link
+    to="/dashboard"
+    class="lg:hidden inline-flex items-center max-w-[min(140px,45vw)] min-w-0 shrink"
+  >
+    <img
+      class="dark:hidden block max-h-10 w-auto max-w-full object-contain object-left"
+      :src="logoUrl('logo.svg')"
+      alt="Logo"
+    />
+    <img
+      class="hidden dark:block max-h-10 w-auto max-w-full object-contain object-left"
+      :src="logoUrl('logo-dark.svg')"
+      alt="Logo"
+    />
   </router-link>
 </template>
 
