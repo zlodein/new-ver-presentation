6b476c2155d29244a974b1d6bebd9ef3ea3e9d35
zlodein <zlodein70@gmail.com>
2026-03-31 21:03:16 +0300

    +v10

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueApexCharts from 'vue3-apexcharts'
+import VueKonva from 'vue-konva'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(VueApexCharts)
+app.use(VueKonva)
 
 app.mount('#app')
